a87de1880f243ae46ebb309405a70c7fda77a9c4
MarinaNetlify <153458673+MarinaNetlify@users.noreply.github.com>
2025-01-14 05:26:44 -0700

    Update personalize.js

--- netlify/edge-functions/personalize.js
+++ netlify/edge-functions/personalize.js
@@ -10,9 +10,9 @@ export default async (request, context) => {
             .on("#netlify-title", {
                 element: (element) => {
                     if (location) {
-                        element.setInnerContent(`Hello from Netlify! How do you enjoy ${location}?`);
+                        element.setInnerContent(`Hello Netlify team! How do you enjoy ${location}?`);
                     } else {
-                        element.setInnerContent(`Hello from Netlify!`);
+                        element.setInnerContent(`Hello Netlify team!`);
                     }
                 },
             })
